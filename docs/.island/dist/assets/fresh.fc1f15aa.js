ce0ec3ab1c70a7a7f1dc37d777d3a8e5217ff219
jiangsongyang <2744856707@qq.com>
2022-11-21 11:19:10 +0800

    island

---
+++ docs/.island/dist/assets/fresh.fc1f15aa.js
@@ -0,0 +1,4 @@
+import{jsx as e,jsxs as o,Fragment as a}from"react/jsx-runtime";const s=void 0,i=[],d="Astro";function r(t){const n=Object.assign({h1:"h1",a:"a",p:"p"},t.components);return o(a,{children:[o(n.h1,{id:"astro",children:[e(n.a,{className:"header-anchor","aria-hidden":"true",href:"#astro",children:"#"}),"Astro"]}),`
+`,e(n.p,{children:"123123"})]})}function h(t={}){const{wrapper:n}=t.components||{};return n?e(n,Object.assign({},t,{children:e(r,t)})):r(t)}const l="Invalid Date",m=`# Astro
+
+123123`;export{m as content,h as default,s as frontmatter,l as lastUpdatedTime,d as title,i as toc};
